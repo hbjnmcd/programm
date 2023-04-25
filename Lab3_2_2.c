df4aaeae468c712236e9cd7737571e89e9960382
hbjnmcd <124435639+hbjnmcd@users.noreply.github.com>
2023-04-25 23:55:34 +0300

    Update Lab3_2_2.c

--- Lab3_2_2.c
+++ Lab3_2_2.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+union prog {
+ unsigned long a;
+ char b[8];
+}
+
+int main(void) {
+  union prog del;
+  del.a = 12391;
+  int i;
+  for (i = 0; i <=7; i++) {
+    printf("%d ", del.b[i]);
+  }
+  return 0;
 }
