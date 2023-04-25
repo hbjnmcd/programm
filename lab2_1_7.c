6c569e7889bbf2c7ca1b2cf843af66d9fcc4ba17
hbjnmcd <124435639+hbjnmcd@users.noreply.github.com>
2023-04-25 21:45:48 +0300

    Add files via upload

---
+++ lab2_1_7.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char **arr = NULL;
+    int i, n;
+    scanf("%d", &n);
+
+    arr = (char **)malloc(sizeof(char *) * n); // массив указателей на строки. Каждую строку надо будет создавать отдельно. И память ей выделять
+
+    for (i = 0; i<n; i++) {
+        char *s;
+        arr[i] = (char*)malloc(1024);
+        arr[i][0] = '\0';
+        scanf("%s", arr[i]);
+        //printf("%s \n", arr[i]);
+    }
+
+    for (i =0; i < n; i++){
+
+        printf("%s \n", arr[i]);
+        free(arr[i]);
+    }
+    free(arr);
+    return 0;
+}
