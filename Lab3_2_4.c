be39aed22cc74a3033c28285b19b2fabffc607a9
hbjnmcd <124435639+hbjnmcd@users.noreply.github.com>
2023-04-26 00:09:49 +0300

    Update Lab3_2_4.c

--- Lab3_2_4.c
+++ Lab3_2_4.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+struct MyUnion {
+  union union_type {
+    int number;
+    char bytes[4];
+  } the_union;
+
+enum {
+  NUMBER,
+  BYTES,
+  } used_type;
+};
+int main (void) {
+ struct MyUnion unions[5];
+  int value;
+  for (int i = 0; i < 5; i++){
+     scanf("%i",&value);
+    unions[i].the_union.number = value;
+    unions[i].used_type = NUMBER;
+  }
+  for (int i = 0; i < 5; i++){
+     printf("%i, ",unions[i].the_union.number);
+    for (int j = 0; j < sizeof(int);j++){
+      printf("%d ", unions[i].the_union.bytes[j] );
+    }
+    printf("\n");
+  }
+  return 0;
 }
