ccd7f7556203e7817aa0e259ae0ad624cc0e1f54
hbjnmcd <124435639+hbjnmcd@users.noreply.github.com>
2023-04-26 00:05:58 +0300

    Update Lab3_3_3.c

--- Lab3_3_3.c
+++ Lab3_3_3.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+enum Week {
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+};
+
+int main (void) {
+    printf("Monday %d\n", Monday);
+    printf("Tuesday %d\n", Tuesday);
+    printf("Wednesday %d\n", Wednesday);
+    printf("Thursday %d\n", Thursday);
+    printf("Friday %d\n", Friday);
+    printf("Saturday %d\n", Saturday);
+    printf("Sunday %d\n", Sunday);
     return 0;
 }
